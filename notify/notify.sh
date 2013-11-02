976f29e6881f7fc061d0fb25af6961c3c569bf4a
Paul Davies C <pauldaviesc@gmail.com>
2013-11-02 08:48:13 +0530

    notify.sh complete

--- notify/notify.sh
+++ notify/notify.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-aureport --key --summary > currop
-diff currop prevop > /dev/null
-if [[ $? -gt 0 ]]
+#This program sends mail using mutt when some new violations happen. Put this script to the crontab of the root user. Also in the root users home folder create a "message" file. This file is sent as the body of the mail. Mutt must be configure for the root user.
+#mailto1 defines whome to the mail has to be sent. You can define another mail and then add it to the mutt command.
+mailto1="pauldaviesc@gmail.com" 
+/sbin/aureport -x --failed --summary -i -if /var/log/audit/audit.log > currop
+
+#Take the diffs of the current and previous output.
+/usr/bin/diff -N --suppress-common-lines currop prevop > diffop 
+
+if [[ $? -gt 0 ]] #If there is a difference
 then
-	#Write the code to send mail here
+	/usr/bin/mutt -s "[VIOLATION] Limits hit Detected" -a diffop <message -- $mailto1 2>/dev/null
 fi
-cp currop prevop
+/bin/cp currop prevop

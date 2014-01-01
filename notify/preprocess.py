e88c4205e175b9afcea71e2cc7a085f03d12c721
Paul Davies C <pauldaviesc@gmail.com>
2014-01-01 07:26:45 +0530

    Ported preprocess in sh to preprocess.py as part of optimization plan with significant reduction in runnint time of notify.sh. With py script was bale to process 5300 violation in less than 3 minutes. Earlier it would have taken more than 20-25 minutes

---
+++ notify/preprocess.py
@@ -0,0 +1,37 @@
+import sys
+import subprocess
+import os
+def main():
+	fp = open(sys.argv[1])
+	if int(sys.argv[3]) == 0:
+		for line in fp:
+			logarray = line.split(' ')
+			sys.stdout.write("%s %s %s %s" %(logarray[3],logarray[4], logarray[7], logarray[8]))
+			selectlogline(sys.argv[2],int(logarray[7].split('=')[1]), float(logarray[2].split('(')[1].split(':')[0]), 4)
+	elif int(sys.argv[3]) == 1:
+		for line in fp:
+			logarray = line.split(' ')
+			sys.stdout.write("%s %s %s %s %s" %(logarray[14],logarray[15],logarray[13], logarray[25], logarray[26]))
+			selectlogline(sys.argv[2],int(logarray[13].split('=')[1]), float(logarray[2].split('(')[1].split(':')[0]), 4)
+def selectlogline(filename ,pid, ts, num_logs):
+	for i in range (0, num_logs):
+		found = 0
+		if i:
+			filename = "/var/log/audit/audit.log.%d" %(i)
+		else:
+			filename = "/var/log/audit/audit.log"
+		afp = open(filename)
+		prevline = ""
+		for i, line in enumerate(afp):
+			if line.startswith("type=SYSCALL") and "key=\"noproc\"" in line and "exit=%d" %(pid) in line :
+				logarray = line.split(' ')
+				currts = float(logarray[1].split('(')[1].split(':')[0])
+				if currts <= ts :
+					prevline = line
+					found = 1
+		if found :
+			logarray = prevline.split(' ')
+			print(" Time Stamp=%f parent_%s parent_%s" %(currts, logarray[24], logarray[25]))
+			break;
+if __name__ == "__main__":
+	main()
